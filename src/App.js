c29e1c74806a6e30f5a489eb649ae82f2cd22ceb
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-04-22 20:47:41 +0400

    final updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,96 @@
 import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/RemoveCircleOutline';
+import AddIcon from '@material-ui/icons/AddCircleOutline';
+import Icon from '@material-ui/core/Icon';
+
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    }
+  },
+  button: {
+    margin: theme.spacing(1),
+  }
+}))
+
 
 function App() {
-  const [inputFields, setInputField] = useState([
-    {firstName: '', lastName: ''}, 
+  const classes = useStyles();
+
+  const [inputFields, setInputFields] = useState([
+    {firstName: '', lastName: ''},
+    {firstName: '', lastName: ''},
   ]);
+
+  const handleChangeInput = (index, event) => {
+    const values = [...inputFields];
+    values[index][event.target.name] = event.target.value;
+    setInputFields(values);
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log("inputFields", inputFields);
+   
+    
+  }
+  
+  const handleAddFields = () => {
+    setInputFields([...inputFields, {firstName: '', lastName: ''}])
+  }
+
+  const handleRemoveFields = (index) => {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  }
+
+
   return (
     <Container>
       <h1>Add New Member</h1>
-      <form>
+      <form className={classes.root} onSubmit = {handleSubmit}>
         { inputFields.map((inputField, index) => (
           <div key={index}>
-            <input type="text" />
+            <TextField 
+            name="firstName"
+            label="First Name"
+            variant = "filled"
+            value={inputField.firstName}
+            onChange = {event => handleChangeInput(index, event)}
+            />
+              <TextField 
+            name="lastName"
+            label="Last Name"
+            variant = "filled"
+            value={inputField.lastName}
+            onChange = {event => handleChangeInput(index, event)}
+            />
+            <IconButton onClick = {handleRemoveFields}>
+              <RemoveIcon />
+            </IconButton>
+            <IconButton onClick = {handleAddFields}>
+              <AddIcon />
+            </IconButton>
             </div>
         ) )}
+         <Button 
+          className = {classes.button}
+          variant="contained" 
+          color="primary"
+          type="submit" 
+          endIcon={<Icon>send</Icon>}
+          onClick={handleSubmit}>
+          Send</Button>
       </form>
+
     </Container>
       
   );
